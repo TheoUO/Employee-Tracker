3e26f57f09ebb34524021aa81e27536e85cf2ec2
TCodes <theodoreuokeke@gmail.com>
2023-08-11 15:08:41 +0100

    added prompts to server.js

---
+++ server.js
@@ -0,0 +1,54 @@
+// needed imports
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const cfonts = require('cfonts');
+
+
+// function to start the employee tracker and prompts
+function start() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role",
+                "Exit",
+            ],
+        })
+        .then((answer) => {
+            switch (answer.action) {
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
+                case "View all roles":
+                    viewAllRoles();
+                    break;
+                case "View all employees":
+                    viewAllEmployees();
+                    break;
+                case "Add a department":
+                    addDepartment();
+                    break;
+                case "Add a role":
+                    addRole();
+                    break;
+                case "Add an employee":
+                    addEmployee();
+                    break;
+                case "Update an employee role":
+                        updateEmployeeRole();
+                        break;
+                case "Exit":
+                    connection.end();
+                    console.log("Have a nice day!");
+                    break;
+            }
+        });
+}
