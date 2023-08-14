143e4fb3c6f4d5d5a14eec158844cabb211045d6
TCodes <theodoreuokeke@gmail.com>
2023-08-14 11:23:13 +0100

    modified seed and schema file

--- db/schema.sql
+++ db/schema.sql
@@ -2,24 +2,31 @@ DROP DATABASE IF EXISTS employeeTracker_db;
 CREATE DATABASE employeeTracker_db;
 USE employeeTracker_db;
 
-CREATE TABLE departments (
+CREATE TABLE department (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 department_name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(255),
 salary DECIMAL(10,2),
-department_id INT,
+department_id INT NOT NULL,
 FOREIGN KEY (department_id)
-REFERENCES departments(id)
+REFERENCES department(id)
+ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
-role_id INT,
-manager_id INT NOT NULL
+role_id INT NOT NULL,
+FOREIGN KEY (role_id)
+REFERENCES role(id)
+ON DELETE CASCADE,
+manager_id INT ,
+FOREIGN KEY (manager_id)
+REFERENCES employee(id)
+ON DELETE SET NULL
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,31 +1,28 @@
-INSERT INTO departments (department_name)
+INSERT INTO department (department_name)
 VALUES 
 ('Sales'),
-('Sales'),
-('Engineering'),
 ('Engineering'),
 ('Finance'),
-('Legal'),
 ('Legal');
 
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO role (title, salary, department_id)
 VALUES 
 ('Sales Lead', 100000, 1),
-('Salesperson', 80000, 2),
-('Lead Engineer', 150000, 3),
-('Software Engineer', 120000, 4),
-('Account Manager', 160000, 5),
-('Accountant', 125000, 6),
-('Legal Team Lead', 75000.00, 7),
-('Lawyer', 185000.00, 8);
+('Salesperson', 80000, 1),
+('Lead Engineer', 150000, 2),
+('Software Engineer', 120000, 2),
+('Account Manager', 160000, 3),
+('Accountant', 125000, 3),
+('Legal Team Lead', 75000.00, 4),
+('Lawyer', 185000.00, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('John', 'Doe', 1, 1),
-('Mike', 'Chan', 2, 2),
-('Ashley', 'Rodriguez', 3, 3),
-('Kevin', 'Tupik', 4, 4),
-('Kumal', 'Singh', 5, 5),
-('Malia', 'Brown', 6, 6),
-('Sarah', 'Lourd', 7, 7),
-('Tom', 'Allen', 8, 8);
+('John', 'Doe', 1, NULL),
+('Mike', 'Chan', 2, 1 ),
+('Ashley', 'Rodriguez', 3, NULL),
+('Kevin', 'Tupik', 4, 3 ),
+('Kumal', 'Singh', 5, NULL),
+('Malia', 'Brown', 6, 5),
+('Sarah', 'Lourd', 7, NULL),
+('Tom', 'Allen', 8, 7);
